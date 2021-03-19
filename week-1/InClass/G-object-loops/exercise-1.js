070c95bf5f6eb7ebfe0ea743b7aff87de93060a6
ArthurNNN <nikitinarthur@gmail.com>
2021-03-20 00:36:43 +0100

    week-1 exersises 4 finished

--- week-1/InClass/G-object-loops/exercise-1.js
+++ week-1/InClass/G-object-loops/exercise-1.js
@@ -3,18 +3,24 @@
 // Try to use both methods shown above to achieve this
 
 const studentGrades = {
-    tom: 20,
-    george: 17,
-    abdul: 19,
-  };
+  tom: 20,
+  george: 17,
+  abdul: 19,
+};
 
 
-  const studentGradesKeys = Object.keys(studentGrades.filter(studentGrade => studentGrade.));
+const studentGradesKeys = Object.keys(studentGrades.filter(studentGrade => studentGrade.value > 18));
 
-  studentGradesKeys.forEach((key) => {
+studentGradesKeys.forEach((key) => {
   console.log(`${key} - ${studentGrades[key]}`);
 });
-  
+
+
+
+for (let key in flowers) {
+  console.log('${flowers[key]}')
+}
+
   // Prints
   // TOM - 20
   // ABDUL - 19
