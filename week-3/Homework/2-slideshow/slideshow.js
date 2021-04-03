9ba30b7bfcd770ce01d8a54ed90c81bb53c86c69
ArthurNNN <nikitinarthur@gmail.com>
2021-04-03 15:28:05 +0200

    week-3 Homework 2-slideshow v2.0

--- week-3/Homework/2-slideshow/slideshow.js
+++ week-3/Homework/2-slideshow/slideshow.js
@@ -4,39 +4,44 @@ const links = ["https:\/\/images.dog.ceo\/breeds\/kelpie\/n02105412_5282.jpg", "
     "https:\/\/images.dog.ceo\/breeds\/schnauzer-giant\/n02097130_6056.jpg", "https:\/\/images.dog.ceo\/breeds\/wolfhound-irish\/n02090721_4032.jpg",
     "https:\/\/images.dog.ceo\/breeds\/germanshepherd\/n02106662_20711.jpg", "https:\/\/images.dog.ceo\/breeds\/bulldog-boston\/n02096585_6940.jpg",
     "https:\/\/images.dog.ceo\/breeds\/eskimo\/n02109961_5597.jpg", "https:\/\/images.dog.ceo\/breeds\/dingo\/n02115641_1380.jpg"];
-let i = 0;
 
+let i = 0;
 postImage(i);
 
 function postImage(i) {
-    i = i % links.length;
     let img = document.querySelector("#image");
-    img.src = links[i % links.length];
+    img.src = links[i];
     let divId = document.querySelector("#divid");
-    divId.textContent = i % links.length;
+    divId.textContent = 'id = ' + i;
 }
 
 document.querySelector("#forward").addEventListener("click", () => {
-    i++;
+    i >= links.length - 1 ? i = 0 : i++;
     postImage(i);
-    console.log(i);
 });
 
 document.querySelector("#back").addEventListener("click", () => {
-    i--;
+    i <= 0 ? i = links.length - 1 : i--;
     postImage(i);
 });
 
 document.querySelector("#forward-auto").addEventListener("click", () => {
-    setInterval(() => {
-        i++;
+    const forward = setInterval(() => {
+        i >= links.length - 1 ? i = 0 : i++;
         postImage(i);
-    }, 5000)
+        document.querySelector("#stop").addEventListener("click", () => {
+            clearInterval(forward);
+        });
+    }, 1000)
 });
 
 document.querySelector("#back-auto").addEventListener("click", () => {
-    setInterval(() => {
-        i--;
+    const backward = setInterval(() => {
+        i <= 0 ? i = links.length - 1 : i--;
         postImage(i);
-    }, 5000)
+        document.querySelector("#stop").addEventListener("click", () => {
+            clearInterval(backward);
+        });
+    }, 1000)
 });
+
